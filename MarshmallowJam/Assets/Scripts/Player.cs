1a404a5737de2608f6794b9ed57aa2f7fc00bce8
Eric Franco <france2@rpi.edu>
2018-11-10 00:24:37 -0500

    Player one and player two movement

---
+++ MarshmallowJam/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    Rigidbody2D rb;
+    public KeyCode[] inputs;
+
+    public virtual void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        MovePlayer();
+	}
+
+    void MovePlayer()
+    {
+        if(Input.GetKeyDown(inputs[3])) //D key
+        {
+            rb.velocity = Vector2.right * speed;
+        }
+        else if(Input.GetKeyDown(inputs[1]))
+        {
+            rb.velocity = Vector2.right * speed * -1f;
+        }
+    }
+}
---
+++ MarshmallowJam/Assets/Scripts/PlayerTwo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwo : Player {
+
+    public override void Awake()
+    {
+        base.Awake();
+        this.inputs = new KeyCode[]{KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow, KeyCode.RightArrow};
+    }
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        base.Update();
+	}
+}
