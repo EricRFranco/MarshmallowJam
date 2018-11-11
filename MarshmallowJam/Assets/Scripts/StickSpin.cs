3c6f14b606d7578465858c385bf1bf6db0083e27
Grant Doney <gdoney1701@gmail.com>
2018-11-10 21:32:21 -0500

    update

--- MarshmallowJam/Assets/Scripts/StickSpin.cs
+++ MarshmallowJam/Assets/Scripts/StickSpin.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StickSpin : MonoBehaviour {
+public class StickSpin : MonoBehaviour
+{
+    public float speed;
+    public float maxRotation;
+    public float maxMove;
+    public float timescale;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        speed = 2f;
+        maxMove = 0f;
+        maxRotation = 0f;
+        timescale = 0f;
+
+    }
+
+    void Update()
+    {
+        timescale += Time.deltaTime *0.005f;
+        //speed += timescale;
+        maxRotation += timescale;
+        maxMove += timescale;
+        transform.rotation = Quaternion.Euler(0f, 0f, maxRotation * Mathf.Sin(Time.time * speed));
+        //transform.position = new Vector3(maxRotation * Mathf.Sin(Time.time * speed), 0f, 0f);
+
+    }
 }
